63af039d411d128710b04cecb2daea3808b9aa85
Christopher Smith <christopher.d.smith4@gmail.com>
2021-05-05 11:58:26 -0700

    Initial commit

---
+++ tell_me_a_fact.py
@@ -0,0 +1,22 @@
+"""
+Mashup to get a random fact from unkno.com, and then say it to you
+"""
+
+import playsound
+import requests
+import os
+from gtts import gTTS
+from bs4 import BeautifulSoup
+
+
+fact_page = requests.get("http://unkno.com/")
+scraped_data = BeautifulSoup(fact_page.text, 'html.parser')
+
+fact = scraped_data.find("div", {"id": "content"})
+
+tts = gTTS(fact.text)
+tts.save("temp.mp3")
+
+playsound.playsound('temp.mp3', True)
+
+os.remove("temp.mp3")
